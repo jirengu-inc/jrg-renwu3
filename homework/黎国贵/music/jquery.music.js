03e784605e69e81905a8bce249b0cec2c898c8a8
Liguogui <tony@gmail.com>
2016-06-27 20:16:59 +0800

    music

--- homework/黎国贵/music/jquery.music.js
+++ homework/黎国贵/music/jquery.music.js
@@ -1,5 +1,6 @@
 function Music() {
     this.clock;
+    this.controlVolumeHide;
     this.songName;
     this.singer;
     this.songPicture;
@@ -25,12 +26,14 @@ function Music() {
 }
 Music.prototype = {
     appendCss : function () {
-        $('head').append('<link rel="stylesheet"  href="http://book.jirengu.com/jirengu-inc/jrg-renwu3/homework/%E9%BB%8E%E5%9B%BD%E8%B4%B5/music/music.css">');
+        $('head').append('<link rel="stylesheet"  href="http://book.jirengu.com/jirengu-inc/jrg-renwu3/homework/%E9%BB%8E%E5%9B%BD%E8%B4%B5/music/Font-Awesome-master/css/music.css">');
         $('head').append('<link rel="stylesheet"  href="http://book.jirengu.com/jirengu-inc/jrg-renwu3/homework/%E9%BB%8E%E5%9B%BD%E8%B4%B5/music/Font-Awesome-master/css/font-awesome.min.css">');
+        $('title').before('<meta name="referrer" content="no-referrer">');
     },
     appendHtml:function () {
         var me = this ;
         var audio = new Audio();
+        //因为me.volumeValue=20就意味着me.audio.volume=1
         audio.volume=0.5;
         me.audio = audio;
         $('body').append(me.audio);
@@ -77,7 +80,9 @@ Music.prototype = {
             '<a href="#" class="song-volume">'+
             '<div class="control-volume">'+
             '<div class="volume-progressbar">'+
-            '<div class="volume-bar"></div>'+
+            '<div class="volume-bar">'+
+                '<div class="volume-bar-circle"></div>'+
+            '</div>'+
             '</div>'+
             '<div class="triangle"></div>'+
             '</div>'+
@@ -109,20 +114,19 @@ Music.prototype = {
     },
     //可移动组件
     dialog:function ($node) {
-        var $node = $node;
         $node.on("mousedown",function (e) {
-            var X = e.pageX - $node.offset().left; //鼠标在dialog内的x轴位置
-            var Y = e.pageY - $node.offset().top; //鼠标在dialog内的y轴位置
-            $node.addClass('cursor').data("inner",{innerX:X,innerY:Y});
+            var X = e.pageX - $node.offset().left;
+            var Y = e.pageY - $node.offset().top;
+            $node.addClass('cursor').data('inner',{x:X,y:Y});
         });
         $node.on("mouseup",function () {
-            $node.removeClass('cursor').data("inner",'');
+            $node.removeClass('cursor').data('inner','');
         });
-        $('body').on("mousemove",function (e) {
-            if($node.data("inner")){
+        $(document).on("mousemove",function (e) {
+            if($node.data('inner')){
                 $node.offset({
-                    left : e.pageX-$node.data("inner").innerX,
-                    top : e.pageY-$node.data("inner").innerY
+                    left : e.pageX-$node.data('inner').x,
+                    top : e.pageY-$node.data('inner').y
                 });
             }
         });
@@ -131,9 +135,9 @@ Music.prototype = {
     toggleShow:function ($switch,$component,event) {
         var $sth = $switch;
         var $cpt = $component;
-        var event = event;
+        var $event = event;
         $sth.data('isShow',false);
-        $sth.on(event,function (e) {
+        $sth.on($event,function (e) {
             if(!$sth.data('isShow')){
                 e.preventDefault();
                 $cpt.show();
@@ -182,14 +186,20 @@ Music.prototype = {
         //音量条的出现
         $('#jquery-music .song-volume').on('mouseenter',function (e) {
             e.preventDefault();
+            $(this).css({
+                cursor:'pointer'
+            });
+            clearTimeout(me.controlVolumeHide);
             $('#jquery-music .control-volume').show();
         });
 
         $('#jquery-music .song-volume').on('mouseleave',function (e) {
             e.preventDefault();
-            $('#jquery-music .control-volume').hide();
+            me.controlVolumeHide = setTimeout(function () {
+                $('#jquery-music .control-volume').hide();
+            },2000);
         });
-        //音量
+        //滚轮控制音量条
         document.getElementsByClassName('song-volume')[0].addEventListener('mousewheel',function (e) {
             //只要滚动就把图标变回来
             if(me.audio.volume === 0){
@@ -212,30 +222,92 @@ Music.prototype = {
             me.audio.volume = me.volumeValue*0.05;
             me.muteVolumeValue = me.volumeValue;
         });
+        //音量条点击事件
+        $('#jquery-music .control-volume').on('click',function (e) {
+            //阻止事件进一步冒泡
+            e.stopPropagation();
+            var a = $('#jquery-music .volume-progressbar').height()-(e.pageY - $('#jquery-music').offset().top-288);
+            $('#jquery-music .volume-bar').height(a);
+            //audio.volume在0与1之间;
+            var b = a*1/$('#jquery-music .volume-progressbar').height();  //多少音量
+            me.audio.volume = b;
+            //因为me.volumeValue=20就意味着me.audio.volume=1
+            me.volumeValue = me.audio.volume*20/1;
+            me.muteVolumeValue = me.volumeValue;
+            if(me.audio.volume === 0){
+                $('#jquery-music .song-volume>i').attr('class','fa fa-volume-off');
+            }
+            if(me.audio.volume < 0.5 && me.audio.volume!==0){
+                $('#jquery-music .song-volume>i').attr('class','fa fa-volume-down');
+            }
+            if(me.audio.volume >= 0.5){
+                $('#jquery-music .song-volume>i').attr('class','fa fa-volume-up');
+            }
+        });
+        //音量条拖拽事件
+        $('#jquery-music .volume-bar-circle').on('mousedown',function (e) {
+            //阻止事件进一步冒泡
+            e.stopPropagation();
+            $('#jquery-music .volume-bar-circle').data('change',true);
+        });
+        $(document).on("mousemove",function (e) {
+            if($('#jquery-music .volume-bar-circle').data("change")){
+                $(this).css({
+                    cursor:'pointer'
+                });
+                var a = $('#jquery-music .volume-progressbar').height()-(e.pageY - $('#jquery-music').offset().top-288);
+                if(a<0){
+                    a = 0;
+                }
+                if(a>$('#jquery-music .volume-progressbar').height()){
+                    a = $('#jquery-music .volume-progressbar').height();
+                }
+                $('#jquery-music .volume-bar').height(a);
+                //audio.volume在0与1之间;
+                var b = a*1/$('#jquery-music .volume-progressbar').height();  //多少音量
+                me.audio.volume = b;
+                //因为me.volumeValue=20就意味着me.audio.volume=1
+                me.volumeValue = me.audio.volume*20/1;
+                me.muteVolumeValue = me.volumeValue;
+                if(me.audio.volume === 0){
+                    $('#jquery-music .song-volume>i').attr('class','fa fa-volume-off');
+                }
+                if(me.audio.volume < 0.5 && me.audio.volume!==0){
+                    $('#jquery-music .song-volume>i').attr('class','fa fa-volume-down');
+                }
+                if(me.audio.volume >= 0.5){
+                    $('#jquery-music .song-volume>i').attr('class','fa fa-volume-up');
+                }
+            }
+        });
+        $('#jquery-music .volume-bar-circle').on('mouseup',function (e) {
+            e.stopPropagation();
+            $('#jquery-music .volume-bar-circle').data('change',false);
+        });
         //静音
         $('#jquery-music .song-volume').on('click',function (e) {
             e.preventDefault();
             if(!$('.song-volume').data('mute')){
                 me.audio.volume = 0;
                 me.volumeValue = 0;
-                $('.volume-bar').height(0);
-                $('.song-volume>i').attr('class','fa fa-volume-off');
-                $('.song-volume').data('mute',true);
+                $('#jquery-music .volume-bar').height(0);
+                $('#jquery-music .song-volume>i').attr('class','fa fa-volume-off');
+                $('#jquery-music .song-volume').data('mute',true);
             }
             else{
                 me.audio.volume = me.muteVolumeValue*0.05;
                 me.volumeValue = me.muteVolumeValue;
-                $('.volume-bar').height(me.volumeValue*4);
+                $('#jquery-music .volume-bar').height(me.volumeValue*4);
                 if(me.audio.volume === 0){
-                    $('.song-volume>i').attr('class','fa fa-volume-off');
+                    $('#jquery-music .song-volume>i').attr('class','fa fa-volume-off');
                 }
                 if(me.audio.volume < 0.5 && me.audio.volume!==0){
-                    $('.song-volume>i').attr('class','fa fa-volume-down');
+                    $('#jquery-music .song-volume>i').attr('class','fa fa-volume-down');
                 }
                 if(me.audio.volume >= 0.5){
-                    $('.song-volume>i').attr('class','fa fa-volume-up');
+                    $('#jquery-music .song-volume>i').attr('class','fa fa-volume-up');
                 }
-                $('.song-volume').data('mute',false);
+                $('#jquery-music .song-volume').data('mute',false);
             }
         });
         //关闭歌词面板
@@ -281,16 +353,23 @@ Music.prototype = {
             clearInterval(me.clock);
             $('#jquery-music .bar-circle').data('change',true);
         });
-        $('body').on("mousemove",function (e) {
+        $(document).on("mousemove",function (e) {
             if($('#jquery-music .bar-circle').data("change")){
                 var a = (e.pageX - $('#jquery-music').offset().left)-10;
+                if(a<0){
+                    a = 0;
+                }
+                if(a>$('#jquery-music .progressbar').width()){
+                    a = $('#jquery-music .progressbar').width();
+                }
                 $('.bar').width(a);
-                me.b = a*me.audio.duration/$('#jquery-music .progressbar').width();  //多少秒
+                me.time = a*me.audio.duration/$('#jquery-music .progressbar').width();  //多少秒
             }
         });
         $('#jquery-music .bar-circle').on('mouseup',function (e) {
             e.preventDefault();
-            me.audio.currentTime = me.b;
+            e.stopPropagation();
+            me.audio.currentTime = me.time;
             me.changeStyle();
             $('#jquery-music .bar-circle').data('change',false);
         });
